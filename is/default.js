4c71dfee64c4cce5af0ce8497a42a3ddd19589d4
baba5998 <babasontyou@ezweb.ne.jp>
2017-06-26 18:01:11 +0900

    Update default.js

--- is/default.js
+++ is/default.js
@@ -1,11 +1,19 @@
-for(var i=1;i<100;i++){
-var li = document.createElement('li');
-li.textContent = i ;
-  if(i%15==0){
-     li.textContent="FizzBuzz";
-  }
-    document.getElementById('list').appendChild(li);
-  }
-  document.getElementById('form').onsubmit = function(){
-    window.alert('document.getElementById('form').word.value');
+for(var i=1;i<=100;i++){
+ var li = document.createElement('li');
+ li.textContent=i;
+ if(i%15==0){
+  
+   li.textContent="FizzBuzz";
+   
+ }else if(i%3==0){
+ li.textContent="Fizz";
+ }else if(i%5==0){
+ li.textContent="Buzz";
+ }
+  document.getElementById('list').appendChild(li);
+  
 }
+
+document.getElementById('form').onsubmit = function(){
+window.alert(document.getElementById('form').word.value);
+};
