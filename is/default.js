e5e76e7b5f02e0c833a77d1fdf0b7d9e66b9b1ba
baba5998 <babasontyou@ezweb.ne.jp>
2017-06-26 18:12:04 +0900

    Update default.js

--- is/default.js
+++ is/default.js
@@ -2,7 +2,13 @@ for(var i=1;i<100;i++){
 var li = document.createElement('li');
 li.textContent = i ;
   if(i%15==0){
-     li.textContent="FizzBuzz";
+ li.textContent="FizzBuzz";
+  }
+  else if(i%3==0){
+  li.textContent="Fizz";
+  }
+   else if(i%5==0){
+  li.textContent="Buzz";
   }
   document.getElementById('list').appendChild(li);
 }
