73f12704ca5aeffc4fb24a0f791e69ba0a0bc49f
baba5998 <babasontyou@ezweb.ne.jp>
2017-06-26 17:22:55 +0900

    Update default.js

--- is/default.js
+++ is/default.js
@@ -1,5 +1,10 @@
+var include_tax = function(price){
+ return Math.floor(list + 0.08 *list);
+}
+
 for(var i=1;i<100;i++){
 var li = documnt.createElement('li');
+  var list = Math.floor(Math.random()*1000);
 li.textContent = i + '回目';
 document.getElementById('list').appendChild(li);
 }
