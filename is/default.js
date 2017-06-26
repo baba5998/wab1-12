40c536775e97c962f887137493a75a905dc8cc4f
baba5998 <babasontyou@ezweb.ne.jp>
2017-06-26 17:27:58 +0900

    Update default.js

--- is/default.js
+++ is/default.js
@@ -1,7 +1,3 @@
-var li = document.creatElement('li');
-li.textContent ='アイテム';
-document.getElementById('list').appendChild;
-
 for(var i=1;i<100;i++){
 var li = documnt.createElement('li');
 li.textContent = i + '回目';
